ea2f5f4ac0e6065b22d112d2d3034fdf238e5f54
Sylvain CHAUGNY <sylvain.chaugny@elevo.io>
2025-03-03 17:55:02 +0100

    Some cleanup

---
+++ src/modes/layout/window.c
@@ -0,0 +1,45 @@
+#include "editor.h"
+#include "utils.h"
+
+void layout_create_new_window(char below) {
+  t_window *window;
+
+  if (window_count() == MAX_WINDOWS) {
+    message("NOPE");
+
+    return ;
+  }
+
+  if (window_count() == 0) {
+    window = window_create(0, 0, E.height - 3, E.width, "coucou");
+  } else {
+    int new_width = E.current_window->cols / (below ? 1 : 2);
+    int new_height = E.current_window->rows / (below ? 2 : 1);
+    int x, y;
+
+    window_resize(E.current_window, new_width, new_height);
+
+    x = E.current_window->x + (below ? 0 : new_width);
+    y = E.current_window->y + (below ? new_height : 0);
+
+    char title[8];
+    sprintf(title, "bla %d", window_count());
+    window = window_create(y, x, new_height, new_width, title);
+  }
+
+  E.current_window = window;
+  E.windows[window_count()] = window;
+}
+
+void layout_destroy_current_window() {
+  t_window *current = E.current_window;
+  int position = window_position(current);
+
+  E.windows[position] = NULL;
+
+  for (int i = position; i < MAX_WINDOWS - 1; i++) {
+    E.windows[i] = E.windows[i + 1];
+  }
+
+  window_destroy(current);
+}
---
+++ src/modes/layout/window.h
@@ -0,0 +1,7 @@
+#ifndef MODES_LAYOUT_WINDOW_H_
+# define MODES_LAYOUT_WINDOW_H_
+
+void layout_create_new_window(char below);
+void layout_destroy_current_window();
+
+#endif
