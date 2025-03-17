0985f7e277c5f279a593777d000f0b7d33fc0498
Asmoddym <sylvain.chaugny@gmail.com>
2025-03-17 22:47:42 +0100

    Adding stuff but not very nicely

--- src/modes/standard/character.c
+++ src/modes/standard/character.c
@@ -7,18 +7,7 @@
 #include "window.h"
 #include "utils.h"
 
-void remove_character_from_line(char **line, int c) {
-  int line_len = strlen(*line);
-
-  for (int i = CW->cursor_x - 1; i < line_len; i++) {
-    (*line)[i] = (*line)[i + 1];
-  }
-  (*line)[line_len - 1] = 0;
-
-  move_cursor(0, -1);
-}
-
-void append_character_to_line(char **line, int c) {
+void append_character_to_line(char **line, int x, int c) {
   int line_len = strlen(*line);
   int new_line_len = line_len + 2;
 
@@ -27,24 +16,60 @@ void append_character_to_line(char **line, int c) {
     *line = realloc(*line, new_line_len);
   }
 
-  for (int i = line_len; i > CW->cursor_x; i--) {
+  for (int i = line_len; i > x; i--) {
     (*line)[i] = (*line)[i - 1];
   }
-  (*line)[CW->cursor_x] = c;
+  (*line)[x] = c;
   (*line)[line_len + 1] = 0;
 
   move_cursor(0, 1);
 }
 
+void concat_line_with_previous(char **line) {
+  int line_len = strlen(*line);
+  char **previous_line = &(CW->file_buffer->lines[window_current_line_index(CW) - 1]);
+  int previous_line_len = strlen(*previous_line);
+
+  for (int x = 0; x < line_len; x++) {
+    append_character_to_line(previous_line, previous_line_len + x, (*line)[x]);
+  }
+
+  for (int y = window_current_line_index(CW); y < CW->file_buffer->lines_number; y++) {
+    CW->file_buffer->lines[y] = CW->file_buffer->lines[y + 1];
+  }
+  CW->file_buffer->lines_number--;
+
+  move_cursor(-1, 0);
+  CW->cursor_x = line_len + previous_line_len;
+}
+
+void remove_character_from_line(char **line) {
+  int line_len = strlen(*line);
+
+  if (CW->cursor_x > 0) {
+    for (int i = CW->cursor_x - 1; i < line_len; i++) {
+      (*line)[i] = (*line)[i + 1];
+    }
+    (*line)[line_len - 1] = 0;
+
+    move_cursor(0, -1);
+  } else {
+    if (CW->cursor_y > 0) {
+      concat_line_with_previous(line);
+    }
+  }
+}
+
 int process_special_character(char **line, int c) {
   switch (c) {
     case KEY_BACKSPACE:
-      remove_character_from_line(line, c);
-
-      return 1;
+      remove_character_from_line(line);
+      break;
     default:
-        return 0;
+      return 0;
   }
+
+  return 1;
 }
 
 void process_character(int c) {
@@ -52,5 +77,5 @@ void process_character(int c) {
 
   if (process_special_character(line, c)) return ;
 
-  append_character_to_line(line, c);
+  append_character_to_line(line, CW->cursor_x, c);
 }
